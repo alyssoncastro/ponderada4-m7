d9349c766597e0cd9141e5f78c1cc90c976f3846
oi <alysson.cordeiro@sou.inteli.edu.br>
2023-10-04 15:18:21 -0300

    alteração

--- fast/model_api.py
+++ fast/model_api.py
@@ -6,39 +6,24 @@ from fastapi import FastAPI
 import uvicorn
 from pydantic import create_model
 
-# Cria a aplicação
+# Create the app
 app = FastAPI()
 
-# Carrega o modelo treinado
+# Load trained Pipeline
 model = load_model("model_api")
 
-# Cria os modelos pydantic de entrada e saída
-cliente_dados = create_model(
-    "model_api_input",
-    client_id=13416,
-    bairro="Mount Olive-Silverstone-Jamestown",
-    FSA="M9V",
-    fonte_de_infecção="Comunidade",
-    data_do_episódio="2020-05-04",
-    sexo_do_cliente="1",
-    resultado="1",
-    hospitalizado_atualmente=0,
-    internado_em_uti_atualmente=0,
-    intubado_atualmente=0,
-    hospitalizado_alguma_vez=0,
-    internado_em_uti_alguma_vez=0,
-    intubado_alguma_vez=0,
-)
-predication = create_model("model_api_output", previsão=1)
+# Create input/output pydantic models
+input_model = create_model("model_api_input", **{'Unnamed: 0': 13416, 'Neighbourhood Name': 'Mount Olive-Silverstone-Jamestown', 'FSA': 'M9V', 'Source of Infection': 'Community', 'Episode Date': '2020-05-04', 'Client Gender': '1', 'Outcome': '1', 'Currently Hospitalized': 0, 'Currently in ICU': 0, 'Currently Intubated': 0, 'Ever Hospitalized': 0, 'Ever in ICU': 0, 'Ever Intubated': 0})
+output_model = create_model("model_api_output", prediction=1)
 
 
-# Define a função de previsão
-@app.post("/predict", response_model=predication)
-def predict(cliente_dados: cliente_dados):
-    cliente_dados = pd.DataFrame([cliente_dados.dict()])
-    previsões = predict_model(model, data=cliente_dados)
-    return {"predication": previsões["predication_label"].iloc[0]}
+# Define predict function
+@app.post("/predict", response_model=output_model)
+def predict(data: input_model):
+    data = pd.DataFrame([data.dict()])
+    predictions = predict_model(model, data=data)
+    return {"prediction": predictions["prediction_label"].iloc[0]}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
